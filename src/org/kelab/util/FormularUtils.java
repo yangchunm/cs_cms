f5c347a266fd532e5efcbdb49c061d62618c2020
MrYang <MrYang@MrYang-PC>
2017-02-08 13:30:52 +0800

    mol文件到svg转换。

--- src/org/kelab/util/FormularUtils.java
+++ src/org/kelab/util/FormularUtils.java
@@ -1,5 +1,7 @@
 package org.kelab.util;
 
+import java.io.File;
+
 import org.openbabel.OBAtom;
 import org.openbabel.OBConversion;
 import org.openbabel.OBMol;
@@ -43,33 +45,18 @@ public class FormularUtils {
 		OBConversion conv = new OBConversion();
 		OBMol mol = new OBMol();
 		String dir = "C:\\Users\\MrYang\\git\\emgen\\WebRoot\\upload\\knmole\\";
-		String srcFile = dir+"1486468470103.mol";
-		String target = dir+"tt.png";
-		conv.SetInFormat("smi");
-		conv.ReadString(mol, "C(Cl)(=O)CCC(=O)Cl");
-		mol.conv
-		OBFormat obf = new OBFormat(0, false);
+		String ext = "mol";
+		String srcFile ="1486468515530.";
+		String src = dir+srcFile+ext;
+		String target = dir+srcFile+"png";
+		conv.SetInFormat(ext);
+		conv.ReadFile(mol,src);
 		// Print out some general information
-		conv.SetOutFormat("can");
-		System.out.print("Canonical SMILES: " +
-		conv.WriteString(mol));
-		System.out.println("The molecular weight is "
-		+ mol.GetMolWt());
-		for(OBAtom atom : new OBMolAtomIter(mol))
-		System.out.println("Atom " + atom.GetIdx()
-		+ ": atomic number = " + atom.GetAtomicNum()
-		+ ", hybridisation = " + atom.GetHyb());
-		// What are the indices of the carbon atoms
-		// of the acid chloride groups?
-		OBSmartsPattern acidpattern = new OBSmartsPattern();
-		acidpattern.Init("C(=O)Cl");
-		acidpattern.Match(mol);
-		vectorvInt matches = acidpattern.GetUMapList();
-		System.out.println("There are " + matches.size() +
-		" acid chloride groups");
-		System.out.print("Their C atoms have indices: ");
-		for(int i=0; i<matches.size(); i++)
-		System.out.print(matches.get(i).get(0) + " ");
+		conv.SetOutFormat("png2");
+		System.out.print("Canonical SMILES: ");
+		conv.AddOption("p");
+		conv.WriteFile(mol, target);
+		
 	}
 	
 	public static void main(String[] args) {
