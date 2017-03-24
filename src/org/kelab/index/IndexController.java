2f59cfc393be7475cf4e717691324ff4fa8e0db9
MrYang <MrYang@MrYang-PC>
2017-03-24 16:59:12 +0800

    首页统计信息完善。

--- src/org/kelab/index/IndexController.java
+++ src/org/kelab/index/IndexController.java
@@ -1,6 +1,7 @@
 package org.kelab.index;
 
 
+import org.kelab.admin.index.IndexAdminService;
 import org.kelab.admin.kn.tree.TreeAdminService;
 
 import com.jfinal.core.Controller;
@@ -8,8 +9,10 @@ import com.jfinal.core.Controller;
 public class IndexController extends Controller{
 	static IndexService srv = new IndexService();
 	static TreeAdminService treeSrv = new TreeAdminService();
+	static IndexAdminService inAdSrv = new IndexAdminService();
 	public void index(){
 		setAttr("knTreeL",treeSrv.findAllKnTree(0));
+		setAttr("static",inAdSrv.siteStatics());
 		render("index.html");
 	}
 	
